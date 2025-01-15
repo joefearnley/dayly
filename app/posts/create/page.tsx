af83f52ac7e93ea04b092b655a3de1894dea7325
Joe Fearnley <joe.fearnley@gmail.com>
2025-01-15 00:19:07 -0500

    work in progress

--- app/posts/create/page.tsx
+++ app/posts/create/page.tsx
@@ -8,7 +8,7 @@ import { createPost } from './actions';
 
 export default async function CreatePost() {
   return (
-    <form className="flex-1 flex flex-col min-w-64">
+    <form className="flex-1 flex flex-col">
       <h1 className="text-2xl font-medium">Create Post</h1>
       <p className="text-sm text-foreground">
         Don't have an account?{" "}
