0341a2975b20bae029c91d81ab5dcf4c8af3a7ac
Joe Fearnley <joe.fearnley@gmail.com>
2025-01-18 08:05:44 -0500

    Moving entry related urls to its own file

--- django/dayly/dayly/urls.py
+++ django/dayly/dayly/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from entries.views import IndexView
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view(), name='index'),
+    path('', include('entries.urls')),
 ]
