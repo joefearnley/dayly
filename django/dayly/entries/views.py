0769f40d937e5e196429fb0b2a7eaba7ee1bd06a
Joe Fearnley <joe.fearnley@gmail.com>
2025-01-18 09:06:04 -0500

    Setting up initial templates and index

--- django/dayly/entries/views.py
+++ django/dayly/entries/views.py
@@ -1,5 +1,11 @@
 from django.views.generic import TemplateView
+from .models import Entry
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['entries'] = Entry.objects.all()[:5]
+        return context
