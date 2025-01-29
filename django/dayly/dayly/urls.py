0b060be6716dfeaf42f25e070eca5ec60841c895
Joe Fearnley <joe.fearnley@gmail.com>
2025-01-28 23:30:15 -0500

    Adding core app and homepage

--- django/dayly/dayly/urls.py
+++ django/dayly/dayly/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('', include('entries.urls')),
+    path('entries/', include('entries.urls')),
+    path('', include('core.urls')),
 ]
