221da1092e8384ed53b946df51b3023be347f3d6
Joe Fearnley <joe.fearnley@gmail.com>
2025-02-01 00:17:36 -0500

    Adding colored validation to forms

--- django/dayly/tailwind.config.js
+++ django/dayly/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import colors from "tailwindcss/colors";
+
 module.exports = {
     content: ["./templates/**/*.{html,js}", "./**/templates/**/*.{html,js}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                ...colors
+            }
+        },
     },
     plugins: [],
 }
