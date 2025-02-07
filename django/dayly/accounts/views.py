0ffcde0ceba3305fad58128be73d41a5d2d890bf
Joe Fearnley <joe.fearnley@gmail.com>
2025-02-06 22:01:31 -0500

    Finishing update account form

--- django/dayly/accounts/views.py
+++ django/dayly/accounts/views.py
@@ -1,13 +1,16 @@
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
+from django.utils.translation import gettext as _
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import UpdateView
 from .forms import AccountUpdateForm
 
 
-class UserUpdateView(LoginRequiredMixin, UpdateView):
+class UserUpdateView(SuccessMessageMixin, LoginRequiredMixin, UpdateView):
     form_class = AccountUpdateForm
     template_name = 'accounts/edit_account.html'
     success_url = reverse_lazy('account-edit')
+    success_message = _('Account updated successfully')
 
     def get_object(self):
         return self.request.user
