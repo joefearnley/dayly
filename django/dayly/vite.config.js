cb8981b7749cb20b80cdb40183a2bae7623ffb52
Joe Fearnley <joe.fearnley@gmail.com>
2025-02-16 00:02:45 -0500

    getting alpine and text editor working

--- django/dayly/vite.config.js
+++ django/dayly/vite.config.js
@@ -2,9 +2,18 @@ import { defineConfig } from "vite";
 import djangoVite from "django-vite-plugin";
 
 export default defineConfig({
+server: {
+        cors: {
+            origin: 'http://127.0.0.1:8000',
+        },
+    },
     plugins: [
         djangoVite({
-            input: ["./static/js/main.js", "./static/css/tailwind.css"],
+            input: [
+                "./static/js/adda-entry.js",
+                "./static/js/main.js",
+                "./static/css/tailwind.css"
+            ],
             pyPath: "python3"
         }),
     ],
