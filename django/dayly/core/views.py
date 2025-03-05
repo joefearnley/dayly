0018c214f958293b8c3feec5d0e0f12fc668282e
Joe Fearnley <joe.fearnley@gmail.com>
2025-03-04 21:17:42 -0500

    Adding icons and view entry template

--- django/dayly/core/views.py
+++ django/dayly/core/views.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
     template_name = 'home.html'
+
+    def get(self, request, *args, **kwargs):
+            if self.request.user.is_authenticated:
+                return HttpResponseRedirect(reverse('entries_index'))
+
+            return super().get(request, *args, **kwargs)
