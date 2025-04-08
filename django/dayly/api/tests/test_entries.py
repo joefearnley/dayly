ed03dfeffa395e90eb6c6c5a990725bfef532c49
Joe Fearnley <joe.fearnley@gmail.com>
2025-04-08 17:52:27 -0400

    Adding the rest of the entry crud tests

--- django/dayly/api/tests/test_entries.py
+++ django/dayly/api/tests/test_entries.py
@@ -8,36 +8,44 @@ from faker import Faker
 from entries.models import Entry
 
 
-class EntriesListTest(APITestCase):
-    def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser',
-            email='test_user_1@gmail.com',
-            password='secret123',
-            first_name='John',
-            last_name='Doe',
-        )
-
-        self.faker = Faker()
-
-        self.token = Token.objects.create(user=self.user)
-        self.token.save()
-
-        self.entries = self.create_entrys(self.user, 3)
-        self.entries_url = '/api/v1/entries/'
-
-    def create_entrys(self, user, count):
-        for i in range(count):
-            date_published = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(weeks=i)
-            slug = date_published.strftime('%Y-%m-%d') + '-' + str(random.choice(range(10000)))
-            Entry.objects.create(
-                body=self.faker.paragraph(),
-                slug=slug,
-                date_published=date_published,
-                user=user,
+class EntriesBaseTest(APITestCase):
+        def setUp(self):
+            self.user = User.objects.create_user(
+                username='testuser',
+                email='test_user_1@gmail.com',
+                password='secret123',
+                first_name='John',
+                last_name='Doe',
             )
 
-        return Entry.objects.filter(user=user)
+            self.faker = Faker()
+
+            self.token = Token.objects.create(user=self.user)
+            self.token.save()
+
+            self.default_date = datetime.datetime.now(datetime.timezone.utc)
+            self.detault_slug = self.default_date.strftime('%Y-%m-%d') + '-' + str(random.choice(range(10000)))
+
+        def create_entrys(self, user, count):
+            for i in range(count):
+                date_published = datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(weeks=i)
+                slug = date_published.strftime('%Y-%m-%d') + '-' + str(random.choice(range(10000)))
+                Entry.objects.create(
+                    body=self.faker.paragraph(),
+                    slug=slug,
+                    date_published=date_published,
+                    user=user,
+                )
+
+            return Entry.objects.filter(user=user)
+
+
+class EntriesListTest(EntriesBaseTest):
+    def setUp(self):
+        super().setUp()
+
+        self.entries_url = '/api/v1/entries/'
+        self.entries = self.create_entrys(self.user, 5)
 
     def test_cannot_list_entries_without_authentication(self):
         response = self.client.get(self.entries_url)
@@ -80,19 +88,35 @@ class EntriesListTest(APITestCase):
             self.assertNotIn(user_entry.body, [e['body'] for e in response.data])
 
 
-class CreateEntryTest(APITestCase):
+class ReadEntryTest(EntriesBaseTest):
     def setUp(self):
-        self.user = User.objects.create_user(
-            username='testuser',
-            email='test_user_1@gmail.com',
-            password='secret123',
-            first_name='John',
-            last_name='Doe',
-        )
+        super().setUp()
 
-        self.faker = Faker()
-        self.default_date = datetime.datetime.now(datetime.timezone.utc)
-        self.detault_slug = self.default_date.strftime('%Y-%m-%d') + '-' + str(random.choice(range(10000)))
+        self.entries_url = '/api/v1/entries/'
+        self.entries = self.create_entrys(self.user, 1)
+
+    def test_user_cannot_read_entry_without_authentication(self):
+        entry = self.entries[0]
+        response = self.client.get(f'{self.entries_url}{entry.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+        self.assertEqual(len(response.data), 1)
+
+    def test_user_can_read_entry(self):
+        entry = self.entries[0]
+        self.client.force_authenticate(user=self.user)
+        response = self.client.get(f'{self.entries_url}{entry.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['body'], entry.body)
+        self.assertEqual(response.data['slug'], entry.slug)
+
+
+class CreateEntryTest(EntriesBaseTest):
+    def setUp(self):
+        super().setUp()
+
         self.entries_url = '/api/v1/entries/'
 
     def test_user_cannot_create_entry_without_authentication(self):
@@ -164,3 +188,126 @@ class CreateEntryTest(APITestCase):
         self.assertEqual(Entry.objects.count(), 0)
         self.assertEqual(response.data['slug'], ['This field is required.'])
 
+
+class UpdateEntryTest(EntriesBaseTest):
+    def setUp(self):
+        super().setUp()
+
+        self.entries = self.create_entrys(self.user, 1)
+        self.entries_url = '/api/v1/entries/'
+
+    def test_user_cannot_update_entry_without_authentication(self):
+        entry = self.entries[0]
+        post_data = {
+            'body': self.faker.paragraph(),
+            'date_published': self.default_date,
+            'slug': self.detault_slug,
+        }
+
+        response = self.client.put(f'{self.entries_url}{entry.id}/', data=post_data)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+        self.assertEqual(len(response.data), 1)
+
+    def test_user_can_update_entry(self):
+        entry = self.entries[0]
+        post_data = {
+            'body': self.faker.paragraph(),
+            'date_published': self.default_date,
+            'slug': self.detault_slug,
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(f'{self.entries_url}{entry.id}/', data=post_data)
+
+        updated_entry = Entry.objects.get(id=entry.id)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(updated_entry.body, post_data['body'])
+        self.assertEqual(updated_entry.date_published, post_data['date_published'])
+        self.assertEqual(updated_entry.slug, post_data['slug'])
+
+    def test_user_cannot_update_entry_without_body(self):
+        entry = self.entries[0]
+        post_data = {
+            'date_published': self.default_date,
+            'slug': self.detault_slug,
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(f'{self.entries_url}{entry.id}/', data=post_data)
+
+        updated_entry = Entry.objects.get(id=entry.id)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(updated_entry.body, entry.body)
+        self.assertEqual(response.data['body'], ['This field is required.'])
+
+    def test_user_cannot_update_entry_without_date_published(self):
+        entry = self.entries[0]
+        post_data = {
+            'body': self.faker.paragraph(),
+            'slug': self.detault_slug,
+        }
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.put(f'{self.entries_url}{entry.id}/', data=post_data)
+
+        updated_entry = Entry.objects.get(id=entry.id)
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(updated_entry.body, entry.body)
+        self.assertEqual(response.data['date_published'], ['This field is required.'])
+
+class DeleteEntryTest(EntriesBaseTest):
+    def setUp(self):
+        super().setUp()
+
+        self.entries = self.create_entrys(self.user, 1)
+        self.entries_url = '/api/v1/entries/'
+
+    def test_user_cannot_delete_entry_without_authentication(self):
+        entry = self.entries[0]
+        response = self.client.delete(f'{self.entries_url}{entry.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.data['detail'], 'Authentication credentials were not provided.')
+        self.assertEqual(len(response.data), 1)
+
+    def test_user_can_delete_entry(self):
+        entry = self.entries[0]
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'{self.entries_url}{entry.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Entry.objects.count(), 0)
+
+    def test_user_cannot_delete_entry_that_does_not_exist(self):
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'{self.entries_url}9999/')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(len(response.data), 1)
+
+    def test_user_cannot_delete_entry_that_belongs_to_another_user(self):
+        other_user = User.objects.create_user(
+            username='testuser2',
+            email='test_user_2@gmail.com',
+            password='secret123',
+            first_name='Jim',
+            last_name='Doe',
+        )
+
+        other_user_entry = Entry.objects.create(
+            body=self.faker.paragraph(),
+            slug=self.detault_slug,
+            date_published=self.default_date,
+            user=other_user,
+        )
+
+        self.client.force_authenticate(user=self.user)
+        response = self.client.delete(f'{self.entries_url}{other_user_entry.id}/')
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(len(response.data), 1)
