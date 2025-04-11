0a9d89e491d2fd3ef7a48853b7d65a28513ca375
Joe Fearnley <joe.fearnley@gmail.com>
2025-04-11 12:17:00 -0400

    getting dashboard to display entries correctly

--- dayly-react/src/pages/Dashboard.jsx
+++ dayly-react/src/pages/Dashboard.jsx
@@ -2,9 +2,9 @@ import { useState } from 'react';
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
+import Markdown from 'react-markdown';
 
 export default function Dashboard() {
-  const [token, setToken] = useState(() => localStorage.getItem('token'));
   const [latestEntries, setLatestEntrie] = useState([]);
   const [loadingLatestEntries, setLoadingLatestEntries] = useState(false);
 
@@ -12,14 +12,16 @@ export default function Dashboard() {
     setLoadingLatestEntries(true);
 
     try {
-      const response = await axios.post(`${import.meta.env.VITE_API_URL}/entries/`, {
+      const token = localStorage.getItem('token');
+      
+      const response = await axios.get(`${import.meta.env.VITE_API_URL}/entries/`, {
         headers: {
           Authorization: `Token ${token}`,
         },
       });
 
-      console.log(`response:`)
-      console.log(response)
+      console.log(`response:`);
+      console.log(response.data);
 
       setLatestEntrie(response.data);
 
@@ -36,7 +38,7 @@ export default function Dashboard() {
 
   return (
     <div className="m-10">
-      <h1 className="text-3xl font-bold">Latest Entries</h1>
+      <h1 className="text-3xl font-bold mb-6">Latest Entries</h1>
       {loadingLatestEntries ? (
         <div className="my-6">
           <span className="">Loading Latest Entries</span>
@@ -45,19 +47,20 @@ export default function Dashboard() {
         ) : (
           <div>
           {latestEntries.length > 0 ? (
-            <div className="overflow-x-auto">
+            <div className="overflow-x-auto latest-entries">
               {latestEntries.map((entry) => (
-                <div key={entry.id} className="overflow-x-auto">
-                  <div>
-                    <Link to="{entry.slug}">{new Date(entry.created_at).toLocaleDateString()}</Link>
-                    <div>{entry.title}</div>
+                <div key={entry.id} className="py-4 border-b latest-entry">
+                  <div className="mb-4">
+                    <Link to={`/entries/${entry.slug}`} className="font-bold">{new Date(entry.date_published).toLocaleDateString()}</Link>
                   </div>
-                  <div>{entry.content}</div>
+                  <div className="body-wrapper">
+                    <Markdown>{entry.body}</Markdown>
+                  </div>
                 </div>
               ))}
             </div>
           ) : (
-            <p>No entries found.</p>
+            <h3 className="my-4 font-medium">No entries found.</h3>
           )}
           </div>
       )}
